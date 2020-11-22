e2927ace04f83a2d5eaef6afee10c7de818fec66
isengrims <kapu4inko@gmail.com>
2020-11-22 21:54:30 +0300

    fix tests

--- TurtleX/TurtleTests/TreeTests.cs
+++ TurtleX/TurtleTests/TreeTests.cs
@@ -11,8 +11,8 @@ namespace TurtleTests
         [TestMethod]
         public void simple_plus_test()
         {
-            IExpression l1 = new Literal(8);
-            IExpression l2 = new Literal(2);
+            IExpression l1 = new Literal(new Token(TokenType.LITERAL, "8", 1, 1));
+            IExpression l2 = new Literal(new Token(TokenType.LITERAL, "2", 1,1));
             IExpression e1 = new OperatorPlus(l1, l2);
 
             Assert.AreEqual(10, e1.eval());
@@ -21,8 +21,8 @@ namespace TurtleTests
         [TestMethod]
         public void simple_minus_test()
         {
-            IExpression l1 = new Literal(8);
-            IExpression l2 = new Literal(2);
+            IExpression l1 = new Literal(new Token(TokenType.LITERAL, "8", 1, 1));
+            IExpression l2 = new Literal(new Token(TokenType.LITERAL, "2", 1, 1));
             IExpression e1 = new OperatorMinus(l1, l2);
 
             Assert.AreEqual(6, e1.eval());
@@ -31,8 +31,8 @@ namespace TurtleTests
         [TestMethod]
         public void simple_multiply_test()
         {
-            IExpression l1 = new Literal(8);
-            IExpression l2 = new Literal(2);
+            IExpression l1 = new Literal(new Token(TokenType.LITERAL, "8", 1, 1));
+            IExpression l2 = new Literal(new Token(TokenType.LITERAL, "2", 1, 1));
             IExpression e1 = new OperatorMultiply(l1, l2);
 
             Assert.AreEqual(16, e1.eval());
@@ -41,8 +41,8 @@ namespace TurtleTests
         [TestMethod]
         public void simple_division_test()
         {
-            IExpression l1 = new Literal(8);
-            IExpression l2 = new Literal(2);
+            IExpression l1 = new Literal(new Token(TokenType.LITERAL, "8", 1, 1));
+            IExpression l2 = new Literal(new Token(TokenType.LITERAL, "2", 1, 1));
             IExpression e1 = new OperatorDivision(l1, l2);
 
             Assert.AreEqual(4, e1.eval());
@@ -51,8 +51,8 @@ namespace TurtleTests
         [TestMethod]
         public void tree_test()
         {
-            IExpression l1 = new Literal(8);
-            IExpression l2 = new Literal(2);
+            IExpression l1 = new Literal(new Token(TokenType.LITERAL, "8", 1, 1));
+            IExpression l2 = new Literal(new Token(TokenType.LITERAL, "2", 1, 1));
             IExpression e1 = new OperatorPlus(l1, l2);
 
             IExpression e2 = new OperatorMinus(l1, l2);
