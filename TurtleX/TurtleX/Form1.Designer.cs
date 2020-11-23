5fa9e40f7dad557c618e4ff75749b694700ba8c0
Chippa99fm <MichVich99fm@gmail.com>
2020-11-23 21:48:48 +0300

    add logo

--- TurtleX/TurtleX/Form1.Designer.cs
+++ TurtleX/TurtleX/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TurtleXForm));
 			this.inputPanel = new System.Windows.Forms.Panel();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
@@ -116,12 +117,13 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(716, 386);
 			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.outputPanel);
 			this.Controls.Add(this.inputPanel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "TurtleXForm";
 			this.Text = "TurtleX";
 			this.Load += new System.EventHandler(this.Form1_Load);
